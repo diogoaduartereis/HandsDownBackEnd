57485e2458184aac9c472259ca879034d42d8f62
diogo <diogoaduartereis@gmail.com>
2020-05-14 22:26:46 +0100

    updated python and files

--- auth.py
+++ auth.py
@@ -7,10 +7,6 @@ auth = Blueprint('auth', __name__)
 def login():
     return render_template('login.html')
 
-@auth.route('/signup')
-def signup():
-    return render_template('signup.html')
-
 @auth.route('/logout')
 def logout():
     return 'Logout'
